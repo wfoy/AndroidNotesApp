075e2d97a99022bfe2f7100f158ba6348d59e29f
Will Foy <wfoy@andrew.cmu.edu>
2019-10-07 00:17:54 -0400

    working!

--- app/src/main/java/com/example/androidnotesapp/MainActivity.java
+++ app/src/main/java/com/example/androidnotesapp/MainActivity.java
@@ -6,41 +6,146 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_CODE = 20;
+    private final ArrayList<String> notes = new ArrayList<String>();
+    ArrayAdapter<String> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<String> notes = new ArrayList<String>();
-        notes.add("test1");
-        notes.add("test2");
+        if(fileExists("__NOTES__")) {
+            String[] entries = open("__NOTES__").split(" ");
+            for(int j = 0; j < entries.length; j++)
+            {
+                if(!entries[j].equals("") && !entries[j].equals(" ")) {
+                    notes.add(entries[j]);
+                }
+            }
+        }
 
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,notes);
+        itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,notes);
 
-        ListView listView = (ListView) findViewById(R.id.list_view);
+        final ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(itemsAdapter);
 
+        final int REQUEST_CODE = 20;
         FloatingActionButton button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, NoteActivity.class);
-                notes.add("Note" + Integer.toString(notes.size()));
-                MainActivity.this.startActivity(intent);
+                intent.putExtra("Title", "Note" + Integer.toString(notes.size()));
+                MainActivity.this.startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+        listView.setClickable(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                String name = listView.getItemAtPosition(position).toString();
+                if(fileExists(name)) {
+                    Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+                    intent.putExtra("Title", name);
+                    intent.putExtra("Words", open(name));
+                    //notes.remove(name);
+                    MainActivity.this.startActivityForResult(intent, REQUEST_CODE);
+                }
             }
         });
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // REQUEST_CODE is defined above
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            // Extract name value from result extras
+            String name = data.getExtras().getString("Title");
+            if(!notes.contains(name) && !name.equals("") && !name.equals(" ")) {
+                notes.add(name);
+            }
+        }
+        itemsAdapter.notifyDataSetChanged();
+    }
+
+    /** Checks if the file denoted by fileName exists. **/
+    private boolean fileExists(String fileName) {
+        File file = getBaseContext().getFileStreamPath(fileName);
+        return file.exists();
+    }
+
+    /** Opens the file denoted by fileName and returns the contents of the file. **/
+    private String open(String fileName) {
+        String content = "";
+        if (fileExists(fileName)) {
+            try {
+                InputStream in = openFileInput(fileName);
+                if ( in != null) {
+                    InputStreamReader tmp = new InputStreamReader( in );
+                    BufferedReader reader = new BufferedReader(tmp);
+                    String str;
+                    StringBuilder buf = new StringBuilder();
+                    while ((str = reader.readLine()) != null) {
+                        buf.append(str + "\n");
+                    } in .close();
+                    content = buf.toString();
+                }
+            } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+                Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+        return content;
+    }
+
+    /** Writes textToSave to the file denoted by fileName. **/
+    private void save(String fileName, String textToSave) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(textToSave);
+            out.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        String toSave = "";
+        for(int i = 0; i < notes.size(); i++)
+        {
+            toSave += notes.get(i) + " ";
+        }
+
+        save("__NOTES__", toSave);
+    }
 
 }
--- app/src/main/java/com/example/androidnotesapp/NoteActivity.java
+++ app/src/main/java/com/example/androidnotesapp/NoteActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidnotesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.OutputStreamWriter;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -19,15 +25,47 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        String title = getIntent().getStringExtra("Title");
+        EditText editText = (EditText)findViewById(R.id.note_title);
+        editText.setText(title, TextView.BufferType.EDITABLE);
+
+        String word = getIntent().getStringExtra("Words");
+        if (word != null) {
+            EditText note = (EditText) findViewById(R.id.note_text);
+            note.setText(word, TextView.BufferType.EDITABLE);
+        }
+
+
+        EditText note_word = (EditText)findViewById(R.id.note_text);
+        note_word.requestFocus();
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent data = new Intent();
+                EditText ttl = (EditText)findViewById(R.id.note_title);
+                EditText note = (EditText)findViewById(R.id.note_text);
+                data.putExtra("Title", ttl.getText().toString());
+                setResult(RESULT_OK, data);
+                save(ttl.getText().toString(), note.getText().toString());
+                finish();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /** Writes textToSave to the file denoted by fileName. **/
+    private void save(String fileName, String textToSave) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(textToSave);
+            out.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 
 }
